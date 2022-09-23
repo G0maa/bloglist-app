ff55eabe3fe0ded5295fe6c4355e063d73bad7fa
G0maa <midomaxgomaa@gmail.com>
2022-09-23 09:06:30 +0200

    Asethtics of .sh files

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-
+echo "### npm install ###"
+echo "cd client, then npm install..."
 cd client
 npm install
 
-cd ..
-cd server
+echo "cd ../server, then npm install..."
+cd ../server
 npm install
--- lint.sh
+++ lint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "### lint ###"
+
+echo "cd client, then npm run lint"
 cd client
 npm run lint
 
+echo "cd ../client, then npm run lint"
 cd ../server
 npm run lint
