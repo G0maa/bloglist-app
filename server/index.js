766aaccc40a4b4722e2ea7c15b6c8427e4fb5ed0
Noriel <N/A>
2022-09-24 09:39:18 +0200

    Making Deta & Development mode work together

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-// const http = require('http')
+const http = require('http')
 const app = require('./app')
-// const config = require('./utils/config')
-// const logger = require('./utils/logger')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-// const server = http.createServer(app)
+const server = http.createServer(app)
 
-// const { PORT } = config
-// server.listen(PORT, () => {
-//   logger.info(`Server running on port ${PORT}`)
-//   logger.info(`URL: http://localhost:${PORT}`)
-//   logger.info(`Running environment: ${config.NODE_ENV}`)
-// })
-
-module.exports = app
+if (config.NODE_ENV) {
+  const { PORT } = config
+  server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
+    logger.info(`URL: http://localhost:${PORT}`)
+    logger.info(`Running environment: ${config.NODE_ENV}`)
+  })
+} else {
+  module.exports = app
+}
