54b45f7c557d02bd90a1d0b186ec77360c6f9ab0
Noriel <N/A>
2022-09-24 08:57:59 +0200

    Fixing deployment #skipChecks

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-const http = require('http')
+// const http = require('http')
 const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+// const config = require('./utils/config')
+// const logger = require('./utils/logger')
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-const { PORT } = config
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-  logger.info(`URL: http://localhost:${PORT}`)
-  logger.info(`Running environment: ${config.NODE_ENV}`)
-})
+// const { PORT } = config
+// server.listen(PORT, () => {
+//   logger.info(`Server running on port ${PORT}`)
+//   logger.info(`URL: http://localhost:${PORT}`)
+//   logger.info(`Running environment: ${config.NODE_ENV}`)
+// })
+
+module.exports = app
